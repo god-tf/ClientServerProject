bc3ff750fe1ff86e35f425cab1ee7fa405568dfc
陶飞 <541003193@qq.com>
2017-11-26 19:39:46 +0800

    初始化程序

--- CommonLibrary/AccountSupport/FormPasswordModify.designer.cs
+++ CommonLibrary/AccountSupport/FormPasswordModify.designer.cs
@@ -33,10 +33,10 @@ namespace CommonLibrary
             this.components = new System.ComponentModel.Container();
             this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.userButton1 = new UserButton();
+            this.userButton1 = new HslCommunication.Controls.UserButton();
             this.panel1 = new System.Windows.Forms.Panel();
             this.label4 = new System.Windows.Forms.Label();
-            this.userButton2 = new UserButton();
+            this.userButton2 = new HslCommunication.Controls.UserButton();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.textBox2 = new System.Windows.Forms.TextBox();
@@ -50,7 +50,7 @@ namespace CommonLibrary
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(163, 20);
+            this.label1.Size = new System.Drawing.Size(243, 28);
             this.label1.TabIndex = 0;
             this.label1.Text = "请输入原密码进行验证：";
             // 
@@ -60,7 +60,7 @@ namespace CommonLibrary
             this.textBox1.Location = new System.Drawing.Point(16, 36);
             this.textBox1.Name = "textBox1";
             this.textBox1.PasswordChar = '*';
-            this.textBox1.Size = new System.Drawing.Size(287, 23);
+            this.textBox1.Size = new System.Drawing.Size(287, 31);
             this.textBox1.TabIndex = 1;
             this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
             // 
@@ -70,7 +70,7 @@ namespace CommonLibrary
             this.userButton1.CustomerInformation = "";
             this.userButton1.EnableColor = System.Drawing.Color.FromArgb(((int)(((byte)(190)))), ((int)(((byte)(190)))), ((int)(((byte)(190)))));
             this.userButton1.Font = new System.Drawing.Font("微软雅黑", 9F);
-            this.userButton1.Location = new System.Drawing.Point(310, 35);
+            this.userButton1.Location = new System.Drawing.Point(310, 40);
             this.userButton1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.userButton1.Name = "userButton1";
             this.userButton1.Size = new System.Drawing.Size(73, 24);
@@ -98,7 +98,7 @@ namespace CommonLibrary
             this.label4.ForeColor = System.Drawing.Color.Gray;
             this.label4.Location = new System.Drawing.Point(4, 164);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(107, 20);
+            this.label4.Size = new System.Drawing.Size(159, 28);
             this.label4.TabIndex = 7;
             this.label4.Text = "剩余修改时间：";
             // 
@@ -122,7 +122,7 @@ namespace CommonLibrary
             this.textBox3.Location = new System.Drawing.Point(8, 124);
             this.textBox3.Name = "textBox3";
             this.textBox3.PasswordChar = '*';
-            this.textBox3.Size = new System.Drawing.Size(287, 23);
+            this.textBox3.Size = new System.Drawing.Size(287, 31);
             this.textBox3.TabIndex = 5;
             // 
             // label3
@@ -130,7 +130,7 @@ namespace CommonLibrary
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(4, 97);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(135, 20);
+            this.label3.Size = new System.Drawing.Size(201, 28);
             this.label3.TabIndex = 4;
             this.label3.Text = "请再次输入新密码：";
             // 
@@ -140,7 +140,7 @@ namespace CommonLibrary
             this.textBox2.Location = new System.Drawing.Point(8, 61);
             this.textBox2.Name = "textBox2";
             this.textBox2.PasswordChar = '*';
-            this.textBox2.Size = new System.Drawing.Size(287, 23);
+            this.textBox2.Size = new System.Drawing.Size(287, 31);
             this.textBox2.TabIndex = 3;
             this.textBox2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox2_KeyDown);
             // 
@@ -149,15 +149,15 @@ namespace CommonLibrary
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(4, 34);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(107, 20);
+            this.label2.Size = new System.Drawing.Size(159, 28);
             this.label2.TabIndex = 2;
             this.label2.Text = "请输入新密码：";
             // 
             // FormPasswordModify
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 28F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(396, 407);
+            this.ClientSize = new System.Drawing.Size(390, 390);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.userButton1);
             this.Controls.Add(this.textBox1);
